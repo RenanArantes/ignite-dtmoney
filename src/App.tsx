ffbbdf2ad89298c16248af2b8cc1e43ee37d6761
RenanArantes <renan.arantes@rede.ulbra.br>
2021-04-19 18:11:10 -0300

    Criando frontend sem backend - Confi o miragejs
    O JSON server cria uma fake API em um arquivo json que tem suas rotas e dados criados de acordo com a estrutura. - O JSON server executa em paralelo a aplicacao(separado). - O Mirage cria uma API fake no frontend que possui DB e podem ser feitos relacionamentos.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
-function App() {
+export function App() {
   return (
     <>
       <Header />
@@ -11,5 +11,3 @@ function App() {
     </>
   );
 }
-
-export default App;
