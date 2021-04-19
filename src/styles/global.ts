922a1f0a9e99c5f28ffb917dbc3f1c22d426acd7
RenanArantes <renan.arantes@rede.ulbra.br>
2021-04-19 17:27:05 -0300

    Componente: Dashboard & Summary

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
   :root {
     --red: #e5234d;
+    --green: #33cc95;
     --blue: #5429cc;
 
     --blue-light: #6933ff;
