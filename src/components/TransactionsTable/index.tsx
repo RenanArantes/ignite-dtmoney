882b9a6912908eeb7bd2f4c56d4e0b740da8cb3e
RenanArantes <renan.arantes@rede.ulbra.br>
2021-04-21 18:18:22 -0300

    Listando transacoes e Seeds

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,22 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { api } from '../../services/api'
 import { Container } from './styles'
 
+interface Transaction {
+  id: number;
+  title: string;
+  ammount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
+
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
   useEffect(() => {
     api.get('transactions')
-    .then(response => console.log(response.data))
+    .then(response => setTransactions(response.data.transactions))
   }, [])
 
   return(
@@ -20,19 +31,17 @@ export function TransactionsTable() {
           </tr>
         </thead>
 
-        <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$ 12.000,00</td>
-            <td>Trabalho</td>
-            <td>01/01/2021</td>
-          </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">-R$ 1.100,00</td>
-            <td>Casa</td>
-            <td>13/01/2021</td>
-          </tr>
+        <tbody>          
+          {transactions.map(transaction => {
+            return (
+              <tr key={transaction.id}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>{transaction.ammount}</td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,38 @@ createServer({
     transaction: Model
   },
 
+  seeds(server) {
+    server.db.loadData({
+      //o nome da table aqui tem que ser no plural
+      //sempre tem que ter id
+      transactions: [
+        {
+          id:1,
+          title:'title 1',
+          type: 'deposit',
+          category: 'Dev',
+          ammount: 6000,
+          createdAt: new Date('2021-02-12 05:00:00'),
+        },
+        {
+          id:2,
+          title:'title 2',
+          type: 'withdraw',
+          category: 'Dev',
+          ammount: 2500,
+          createdAt: new Date('2021-02-13 08:20:00'),
+        },
+        {
+          id:3,
+          title:'title 3',
+          type: 'deposit',
+          category: 'Dev',
+          ammount: 500,
+          createdAt: new Date('2021-02-14 09:00:00'),
+        }
+      ]
+    })
+  },
 
   routes() {
     this.namespace = 'api';
